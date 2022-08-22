8f5d80c7448ceb35884d9cf5969c4659bdd2714d
PavlovNick <pavlovmikola01@gmail.com>
2022-08-22 14:41:22 +0300

    add all files

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function closeFacebookAnalysis__form(){
+    var facebookAnalysis_form_ = document.getElementById('facebookAnalysis_form_');
+    if(facebookAnalysis_form_.style.display === 'block')
+    {
+        facebookAnalysis_form_.style.display = 'none';
+    }
+    else
+    {
+        facebookAnalysis_form_.style.display = 'block';
+    }
+}
